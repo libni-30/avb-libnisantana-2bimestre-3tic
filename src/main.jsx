496e5c7b14f73d6a25d9d7df7600c681a1960cda
libnisntn@gmail.com <libnisntn@gmail.com>
2025-05-13 12:11:18 -0300

    api install

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home2 from './pages/Home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Home2></Home2>
 )
